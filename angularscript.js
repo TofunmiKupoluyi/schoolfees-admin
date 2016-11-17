e04541aaa6181b8f64b8d89f858d8b5271539223
tofunmi kupoluyi <tofunmi kupoluyi>
2016-11-17 19:29:27 +0100

    Initial stable commit

---
+++ angularscript.js
@@ -0,0 +1,416 @@
+var app= angular.module("myApp", ['ngRoute','chart.js']);
+var editData="";
+app.config(function($routeProvider){
+$routeProvider
+
+.when("/", {
+    templateUrl:"home.html",
+    controller:"HomeController"
+})
+.when("/form", {
+    templateUrl:"form.html",
+    controller:"FormController"
+})
+
+.when("/data", {
+    templateUrl:"data.html",
+    controller:"DataController"
+})
+.when("/edit", {
+    templateUrl:"edit.html",
+    controller:"EditController"
+})
+.when("/settings", {
+    templateUrl:"settings.html",
+    controller:"SettingsController"
+})
+.when("/analytics", {
+    templateUrl:"analytics.html",
+    controller:"AnalyticsController"
+})
+.otherwise({
+    redirectTo:"/"
+});
+
+});
+
+
+app.controller("HomeController", function($scope){
+    $scope.message="Welcome to my single page web-app utilizing the capabilities of Node's REST API";
+});
+
+
+app.service("dataServices", function($http, $window, $q){
+    var narrations="";
+    
+    this.getNarration= function(){
+        var deferred= $q.defer();
+        $http.get("/data/narration").then(function(response){
+            var narrations= response.data.narrations;
+            if(narrations){
+                deferred.resolve(narrations);
+            }
+            else{
+                deferred.reject("No narrations available");
+            }
+        });
+        return deferred.promise;
+    };
+
+    this.getFees= function(){
+        var deferred= $q.defer();
+        $http.get("/data/fees").then(function(response){
+            var fees= response.data.fees;
+            if(fees){
+                deferred.resolve(fees);
+            }
+            else{
+                deferred.reject("No fees available");
+            }
+        });
+        return deferred.promise;
+    };
+});
+
+app.controller("FormController", function($scope, $http, dataServices){
+   dataServices.getNarration().then(function(response){
+       $scope.narrations=response;
+   });
+    $scope.sendForm= function(){
+        var data={
+            payerfirstname: "N/A",
+            payerlastname: "N/A",
+            payeremail: "N/A",
+            payerrelationship: "N/A",
+            studentfirstname: $scope.studentfirstname,
+            studentlastname: $scope.studentlastname,
+            studentclass: $scope.studentclass,
+            studentarm: $scope.studentarm,
+            studentid: $scope.studentid,
+            amountpaid: $scope.amountpaid,
+            narration: $scope.narration
+        }
+        $http.post("/data", data).then(
+            function(response){
+                $scope.message= response.data.products;
+                if($scope.message=="Input successful"){
+                    window.location="#/data";
+                }
+            }
+        );
+    };
+    
+});
+
+
+app.controller("DataController", function(dataServices, $scope, $http){
+    dataServices.getNarration().then(function(response){
+        $scope.narrations= response;
+        console.log(response);
+    });
+    
+    $scope.message="There is an error";
+    $scope.buttonClickable="disabled";
+    $http.get("/data").then(function(response){
+        $scope.message= response.data.products;
+    });
+
+     $scope.productClicked= function(idreference, id, studentfirstname, studentlastname, studentclass, studentarm, status){
+        $scope.idreference= idreference;
+        $scope.studentid= id;
+        $scope.studentfirstname= studentfirstname;
+        $scope.studentlastname=studentlastname;
+        $scope.studentclass=studentclass;
+        $scope.studentarm= studentarm;
+        $scope.status= status;
+        $scope.buttonClickable="btn-primary";   
+        window.scrollTo(0,0);     
+    };
+
+    $scope.editClicked= function(idreference,id, studentfirstname, studentlastname, studentclass, studentarm, status){
+        editData={
+            idreference: idreference,
+            studentid: id,
+            studentfirstname: studentfirstname,
+            studentlastname: studentlastname, 
+            studentclass: studentclass,
+            studentarm: studentarm,
+            status: status
+        };
+        window.location="#/edit";
+    };
+
+    $scope.filter= function(){
+        var filter= $scope.searchclass;
+        var searchquery= $scope.searchQuery;
+        var issuccessful= $scope.issuccessful;
+        var searcharm=$scope.searcharm;
+        var narration=$scope.narration;
+        $http.get("/data/filterclass?filter="+filter+"&searchquery="+searchquery+"&issuccessful="+issuccessful+"&searcharm="+searcharm+"&narration="+narration).then(function(response){
+            $scope.message= response.data.products;
+        });
+    }
+    
+
+});
+
+app.controller("EditController", function($scope, $http){
+    $scope.idreference=editData.idreference;
+    $scope.id=editData.id;
+    $scope.studentfirstname= editData.studentfirstname;
+    $scope.studentlastname= editData.studentlastname;
+    $scope.studentclass= editData.studentclass;
+    $scope.studentid= editData.studentid;
+    $scope.studentarm= editData.studentarm;
+    $scope.status= editData.status;
+    
+    $scope.alterClick= function(){
+        var newData={
+            idreference: $scope.idreference,
+            id:$scope.id,
+            studentfirstname: $scope.studentfirstname,
+            studentlastname: $scope.studentlastname,
+            studentclass: $scope.studentclass,
+            studentid: $scope.studentid,
+            studentarm: $scope.studentarm,
+            status: $scope.status,
+        }
+        $http.post("/data/update", newData).then(function(response){
+            $scope.message=response.data.products;
+        });
+    };
+
+    $scope.deleteClick = function(){
+        var deleteData={
+            idreference: $scope.idreference,
+            id:$scope.id,
+            studentfirstname: $scope.studentfirstname,
+            studentlastname: $scope.studentlastname,
+            studentclass: $scope.studentclass,
+            studentid: $scope.studentid,
+            studentarm: $scope.studentarm,
+            status: $scope.status,
+        };
+        $http.post("data/delete", deleteData).then(function(response){
+            $scope.message= response.data.products;
+        });
+            $scope.idreference="";
+            $scope.id="";
+            $scope.studentfirstname="";
+            $scope.studentlastname="";
+            $scope.studentclass="";
+            $scope.studentid="";
+            $scope.studentarm="";
+            $scope.status="";
+    };
+    $scope.backClick=function(){
+        window.location="#/data";
+    };
+});
+
+app.service("updateFeeService", function($http, $q){
+    this.sendFees= function(data){
+        var deferred= $q.defer();
+        $http.post("/data/updateFees", data).then(function(response){
+            if(response){
+                deferred.resolve(response.data.newArray);
+            }
+            else{
+                deferred.reject("Error");
+            }
+        });
+        return deferred.promise;
+    };
+
+    this.addFees= function(data){
+        var deferred= $q.defer();
+        $http.post("/data/addFees", data).then(function(response){
+            if(response){
+                console.log(response.data);
+                deferred.resolve(response.data.newArray);
+            }
+            else{
+                deferred.reject("Error");
+            }
+        });
+        return deferred.promise;
+    }
+
+});
+
+app.controller("SettingsController", function($scope, $http, dataServices, updateFeeService){
+    $scope.newRows=[];
+    dataServices.getFees().then(function(response){
+        $scope.fees=response;
+        
+    });
+    $scope.visible=false;
+    $scope.onClick=function(){
+        $scope.visible=true;
+        $scope.updateName="Update";
+    };
+
+    $scope.update= function(){
+        var fees= $scope.fees;
+        $scope.activeButton="disabled";
+        var data={
+            fees:fees
+        };
+        updateFeeService.sendFees(data).then(function(response){
+            $scope.fees= response;
+            $scope.activeButton="";
+            $scope.visible=false;
+        });
+    };
+
+    
+    $scope.addNewRowClick= function(){
+        var blankData= {
+            fees:"",
+            narration:""
+        }
+        $scope.newRows.push(blankData);
+    };
+
+    $scope.onNewClick = function(){
+        $scope.newVisible=true;
+        $scope.newName="Add Fee";
+    };
+
+    $scope.addRow= function(){
+        var fees= $scope.newRows;
+        $scope.activeButton="disabled";
+        var data={
+            fees:fees
+        };
+        updateFeeService.addFees(data).then(function(response){
+            console.log(response);
+            $scope.newRows=[];
+            $scope.fees= response;
+            $scope.newVisible=false;
+            $scope.activeButton="";
+        });
+    }
+});
+
+app.service("analyticsService", function($http, $q){
+    function getTransactions(filter){
+        var deferred= $q.defer();
+        if(filter){
+            $http.get("/data/transactions?filter="+filter).then(function(response){
+                if(response){
+                    deferred.resolve(response);
+                }
+                else{
+                    deferred.reject("No response");
+                } 
+            });
+            return deferred.promise;
+        }
+        else{
+            $http.get("/data/transactions").then(function(response){
+                if(response){
+                    deferred.resolve(response);
+                }
+                else{
+                    deferred.reject("No response");
+                }
+            });
+            return deferred.promise;
+        }
+    };
+
+    this.getTransactions= function(filter){
+        var deferred= $q.defer();
+        getTransactions(filter).then(function(response){
+            if(response){
+                deferred.resolve(response);
+            }
+            else{
+                deferred.reject("Error, no response");
+            }
+        });
+        return deferred.promise;   
+    };
+
+    this.orderTransactions= function(filter){
+        var deferred= $q.defer();
+        getTransactions(filter).then(function(response){
+            var total=[0,0,0,0,0,0];
+            if(response){
+                for(var i=0; i<response.data.response.length;i++){
+                    switch(response.data.response[i].studentclass){
+                        case 1: 
+                            total[0]+=response.data.response[i].amountpaid;
+                        break;
+                        case 2: 
+                            total[1]+=response.data.response[i].amountpaid;
+                        break;
+                        case 3: 
+                            total[2]+=response.data.response[i].amountpaid;
+                        break;
+                        case 4: 
+                            total[3]+=response.data.response[i].amountpaid;
+                        break;
+                        case 5: 
+                            total[4]+=response.data.response[i].amountpaid;
+                        break;
+                        case 6: 
+                            total[5]+=response.data.response[i].amountpaid;
+                        break;   
+                    } 
+                }
+                deferred.resolve(total);   
+            }
+            else{
+                deferred.reject("Error in arrangement");
+            } 
+        });
+        return deferred.promise;
+    };
+
+
+    
+});
+
+app.controller("AnalyticsController", function($scope, dataServices, analyticsService){
+    dataServices.getNarration().then(function(response){
+        $scope.narrations= response;
+    });
+    analyticsService.getTransactions().then(function(response){
+        console.log(response);
+        $scope.successfulCount= response.data.successfulCount;
+        $scope.unsuccessfulCount= response.data.unsuccessfulCount;
+        $scope.successfulAmount= accounting.formatMoney(response.data.successfulAmount, "₦ ", 2, " ", ".");
+    });
+    analyticsService.orderTransactions().then(function(total){
+        $scope.data=[total];
+    });
+
+    $scope.options={
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true   
+                }
+            }]
+        }
+    }
+    $scope.chartType="bar";
+    $scope.labels =["Year 1", "Year 2", "Year 3", "Year 4", "Year 5", "Year 6"];
+
+    $scope.getNarrationClick= function(narration){
+        analyticsService.getTransactions(narration).then(function(response){
+            console.log(response);
+            $scope.successfulCount= response.data.successfulCount;
+            $scope.unsuccessfulCount= response.data.unsuccessfulCount;
+            $scope.successfulAmount= accounting.formatMoney(response.data.successfulAmount, "₦ ", 2, " ", ".");
+        });
+        analyticsService.orderTransactions(narration).then(function(total){
+            $scope.data=[total];
+        });
+    };
+
+    
+});
---
+++ app.js
@@ -0,0 +1,387 @@
+var express= require("express");
+var mysql= require("mysql");
+var bodyParser= require("body-parser");
+var app=express();
+var router= express.Router();
+app.use("images", express.static("./images"));
+app.use("dependencies", express.static("./node_modules"))
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.get("/", function(req,res){
+    res.sendfile("index.html");
+});
+app.use("/data", router);
+var connection= mysql.createConnection({
+    host:'localhost',
+    user:"root",
+    password:"",
+    database:"schoolfees"
+});
+
+
+
+router.post("/", function(request, response){
+    var payerfirstname= request.body.payerfirstname;
+    var payerlastname= request.body.payerlastname;
+    var payeremail= request.body.payeremail;
+    var payerrelationship= request.body.payerrelationship;
+    var studentfirstname= request.body.studentfirstname;
+    var studentlastname= request.body.studentlastname;
+    var studentclass= request.body.studentclass;
+    var studentarm= request.body.studentarm;
+    var studentid= request.body.studentid;
+    var amountpaid= request.body.amountpaid;
+    var narration= request.body.narration;
+    var d = new Date();
+    var year = d.getFullYear();
+    var day= d.getDate();
+    var month= d.getMonth()+1;
+    var fullDate= year+"-"+month+"-"+day;
+    var items={
+        payerfirstname: payerfirstname,
+        payerlastname: payerlastname,
+        payeremail: payeremail,
+        payerrelationship: payerrelationship,
+        studentfirstname: studentfirstname,
+        studentlastname: studentlastname,
+        studentclass: studentclass,
+        studentarm: studentarm,
+        studentid: studentid,
+        amountpaid: amountpaid,
+        narration: narration,
+        status: "Successful",
+        datepaid:fullDate
+    };
+    var data={
+        error:1,
+        products:""
+    };
+    connection.query("INSERT INTO payments SET ?", items, function(err, res){
+        if(err){
+            data.error=1;
+            data.products="Error in code";
+            console.log("Error in code "+err);
+            response.json(data);
+        }
+        else{
+            data.error=0;
+            data.products="Input successful";
+            console.log("Data inserted");
+            response.json(data);
+        }
+    });
+});
+
+router.get("/", function(request, response){
+    var data={
+        error:1,
+        products:""
+    };
+    connection.query("SELECT * FROM payments ORDER BY id DESC", function(err, rows, fields){
+        if(rows){
+            if(rows.length>0){
+                data.products=rows;
+                for(var i=0; i<rows.length; i++){
+                    var d = new Date(rows[i]["datepaid"]);
+                    console.log(d);
+                    var year = d.getFullYear();
+                    var day= d.getDate();
+                    var month= d.getMonth()+1;
+                    var fullDate= year+"-"+month+"-"+day;
+                    rows[i]["datepaid"]=fullDate;
+                }
+            
+                data.error=0;
+                response.json(data);
+            }
+            else{
+                data.products="No products found";
+                data.error=1;
+                response.json(data);
+            }
+        }
+        else{
+            data.products="Error";
+            data.error=1;
+            response.json(data);
+        }
+    });
+    
+});
+
+router.post("/update", function(request, response){
+    var data={
+        error:1,
+        products:""
+    };
+    var idreference= request.body.idreference;
+    var studentfirstname= request.body.studentfirstname;
+    var studentlastname= request.body.studentlastname;
+    var studentclass= request.body.studentclass;
+    var studentarm= request.body.studentarm;
+    var studentid= request.body.studentid;
+    connection.query("UPDATE payments SET studentfirstname=? , studentlastname=?, studentclass=?, studentarm=?, studentid=? WHERE ID=?",[studentfirstname, studentlastname, studentclass, studentarm, studentid, idreference], function(err, res){
+        if(err){
+            data.error=1;
+            data.products="Error in code "+err;
+            response.json(data);
+        }
+        else{
+
+            data.error=0;
+            data.products="Data update successful";
+            console.log(res);
+            response.json(data);
+        }
+    });
+});
+
+router.get("/narration", function(request, response){
+    console.log("Welcome to narrations");
+    var data={
+        error:1,
+        narrations:""
+    }
+    connection.query("SELECT narration FROM fees", function(err, res){
+        if(err){
+            data.error=1;
+            data.narrations="No narrations";
+            response.json(data);
+        }
+        else{
+            data.error=0;
+            data.narrations=res;
+            response.json(data);
+        }
+    });
+});
+
+router.get("/fees", function(request, response){
+    console.log("Welcome to fees");
+    var data={
+        error:1,
+        fees:""
+    }
+    connection.query("SELECT * FROM fees", function(err, res){
+        if(err){
+            data.error=1;
+            data.fees="No fees";
+            response.json(data);
+        }
+        else{
+            data.error=0;
+            data.fees=res;
+            response.json(data);
+        }
+    });
+
+});
+
+router.post("/updateFees", function(request, response){
+    var data={
+        error:1,
+        newArray: ""
+    }
+    var receivedData= request.body.fees;
+    console.log(receivedData);
+    for(var i=0; i<receivedData.length; i++){
+        var id= receivedData[i]["id"];
+        var narration= receivedData[i]["narration"];
+        var fees= receivedData[i]["fees"];
+        connection.query("UPDATE fees SET narration=?, fees=? WHERE id=?",[narration, fees, id], function(err, res){
+            if(err){
+                data.newArray=[];
+                data.error=1;
+                
+            }
+            else{
+                console.log("Here");
+                data.error=0;
+            }
+        });
+    }
+    data.error=0;
+    data.newArray= receivedData;
+    response.json(data);
+
+});
+
+router.post("/addFees", function(request, response){
+    
+    var receivedData= request.body.fees;
+    function addFeesFunction(){
+        var data= {
+            error:1,
+            newArray: ""
+        };
+
+        for(var i=0; i<receivedData.length;i++){
+            connection.query("INSERT INTO fees SET ?", [receivedData[i]], function(err, res){
+                if(err){
+                    data.error=1;
+                    data.newArray="Error in code "+err;
+                    console.log(data);
+                    response.json(data);
+                    console.log("THEERE IS AN ERROR O");
+                    return;
+                }
+                
+            });
+            
+        }       
+        connection.query("SELECT * FROM fees", function(err, res){
+            
+            if(err){
+                data.error=1;
+                data.newArray="Error in second step of code";
+                console.log(data);
+            }
+            else{
+                data.error=0;
+                data.newArray=res;
+                console.log(data);
+                response.json(data);
+            }
+        });
+    }
+    addFeesFunction();
+});
+
+        
+
+router.get("/filterclass", function(request, response){
+    console.log("We're here");
+    var data={
+        error:1,
+        products: ""
+    }
+    if(request.query.filter!="undefined"){
+        var classQuery= request.query.filter+"%";
+    }
+    else{
+        var classQuery="%";
+    }
+    if(request.query.searchquery!="undefined"){
+        var searchQuery= request.query.searchquery+"%";
+    }
+    else{
+        var searchQuery= "%";
+    }
+    if(request.query.searcharm!="undefined"){
+        var searcharm= request.query.searcharm+"%";
+    }
+    else{
+        var searcharm="%"
+    }
+    if(request.query.issuccessful!="undefined"){
+        var issuccessful= request.query.issuccessful+"%";
+    }
+    else{
+        var issuccessful="%"
+    }
+    if(request.query.narration !="undefined"){
+        var narration= request.query.narration+"%";
+    }
+    else{
+        var narration="%";
+    }
+    
+    console.log(searchQuery);
+    console.log(classQuery);
+    console.log(issuccessful);
+    console.log(searcharm);
+    connection.query("SELECT * FROM payments WHERE (studentfirstname LIKE ? OR studentlastname LIKE ? OR studentid LIKE ?) AND (studentclass LIKE ?) AND (status LIKE ?) AND (studentarm LIKE ?) AND (narration LIKE ?) ORDER BY id DESC", [searchQuery, searchQuery, searchQuery, classQuery, issuccessful, searcharm, narration], function(err, res){
+        if(err){
+            data.error=1;
+            data.products="Error in code"+err;
+            response.json(data);
+        }
+        else{
+            
+            for(var i=0; i<res.length; i++){
+                    var d = new Date(res[i]["datepaid"]);
+                    console.log(d);
+                    var year = d.getFullYear();
+                    var day= d.getDate();
+                    var month= d.getMonth()+1;
+                    var fullDate= year+"-"+month+"-"+day;
+                    res[i]["datepaid"]=fullDate;
+                }
+            data.error=0;
+            data.products=res;
+            response.json(data);
+        }
+    }); 
+});
+router.post("/delete", function(request, response){
+     var data={
+        error:1,
+        products:""
+    };
+    var idreference=request.body.idreference;
+
+    connection.query("DELETE FROM payments WHERE id=?", [idreference], function(err, res){
+        if(err){
+            data.error=1;
+            data.products="Error deleting";
+            console.log(error);
+            response.json(data);
+        }
+        else{
+            data.error=0;
+            data.products="Row has been removed";
+            console.log(res);
+            response.json(data);
+        }
+    });
+
+});
+
+router.get("/transactions", function(request, response){
+    if(request.query.filter){
+        var filter=request.query.filter+"%";
+    }
+    else{
+        var filter="%";
+    }
+    
+    console.log(filter);
+    var data={
+        error:1,
+        response:"",
+        successfulCount:0,
+        unsuccessfulCount:0,
+        successfulAmount:0
+    };
+    connection.query("SELECT status, studentclass, narration, amountpaid FROM payments WHERE narration LIKE ?",[filter], function(err, rows){
+        if(err){
+            data.error=1;
+            data.response="Error utilizing code";
+            console.log(data);
+            response.json(data);
+            
+        }
+        else{
+            data.error=0;
+            
+            for(var i=0; i<rows.length;i++){
+                if(rows[i].status=="Successful"){
+                    data.successfulCount+=1;
+                    data.successfulAmount+= rows[i].amountpaid;
+                }
+                else{
+                    data.unsuccessfulCount+=1;
+                }
+            }
+            data.response=rows;
+            response.json(data);
+        }
+    });
+
+    
+
+});
+app.listen(3000);
